167606c17f6f751b42ec9b8184ef82b3a3f0495a
AmandaSuyelen <amandasuyelen.1@yahoo.com>
2025-07-31 16:03:29 -0300

    Updated UnifiedAttendancePage.tsx

--- src/components/UnifiedAttendancePage.tsx
+++ src/components/UnifiedAttendancePage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Plus, Search, Package, Scale, ShoppingCart, Calculator, User, DollarSign, Trash2, Minus } from 'lucide-react';
-import { usePDVProducts, usePDVSales, usePDVCart } from '../../hooks/usePDV';
+import { usePDVProducts, usePDVSales, usePDVCart } from "../hooks/usePDV";
 import { usePDVCashRegister } from '../../hooks/usePDVCashRegister';
 import { PDVOperator, PDVProduct } from '../../types/pdv';
 import { PesagemModal } from "../PDV/PesagemModal";
